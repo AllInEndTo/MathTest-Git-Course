635f8114709766672f4c9a29056a775ddfddcee8
Semyon Beloplotov <beloplotov.s@mail.ru>
2022-11-26 01:00:23 +0500

    first message, let's see what's we'll get

---
+++ script.js
@@ -0,0 +1,30 @@
+let button = document.getElementById("finish");
+
+button.addEventListener("click", checkTest);
+
+function checkTest() {
+    let result = 0;
+
+    let a1 = document.getElementById('q1').value;
+    if(a1 === '63'){
+        result++;
+    }
+    let a2 = document.getElementById('q2').value;
+    if(a2 === '0'){
+        result++;
+    }
+    let a3 = document.getElementById('q3').value;
+    if(a3 === '144'){
+        result++;
+    }
+    let a4 = document.getElementById('q4').value;
+    if(a4 === '9'){
+        result++;
+    }
+    let a5 = document.getElementById('q5').value;
+    if(a5 === '6'){
+        result++;
+    }
+
+    alert(`Количество правильных ответов: ${result}`)
+}
